cce0ca0ca0c4141ba756537e382ff4586f7b4306
sevenapik <32540285+sevenapik@users.noreply.github.com>
2017-10-05 13:32:10 +0300

    Add files via upload

---
+++ вк/offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1507191535,
+	"fileList": [
+		"data.js",
+		"game.html",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/ur-sheet0.png",
+		"images/heroes-sheet0.png",
+		"images/zombie-sheet0.png",
+		"images/bulet-sheet0.png",
+		"images/logo-sheet0.png",
+		"images/ur1-sheet0.png",
+		"images/shot-sheet0.png",
+		"images/jump-sheet0.png",
+		"images/levo-sheet0.png",
+		"images/pravo-sheet0.png",
+		"images/restart-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sprite3-sheet0.png",
+		"media/002.ogg",
+		"media/zander_noriega_-_fight_them_until_we_cant.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
